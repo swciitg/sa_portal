fa8f9a6b531753d5409c44b7e694451e34d098db
ptayal76 <p.tayal@iitg.ac.in>
2023-04-27 01:27:39 +0530

    Event card and carousel added

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      height : {
+        'laptopimg' : '60vh',
+        'mobileimg' : '40vh'
+      },
+      width : {
+        'lapimg' : '80vh'
+      }
     },
   },
   plugins: [],
