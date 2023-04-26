1c242e38cce6189b2791d1926f0114582e8f22d8
Raghav <raghav03.doda@gmail.com>
2023-04-26 20:03:49 +0530

    Navbar and Card components added

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
@@ -12,7 +14,13 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      screens: {
+        'rag': '840px',
+        ...defaultTheme.screens,
+      },
     },
   },
   plugins: [],
 }
+
+      
