b294d8d83fed8e77f5f20eb979a6fc88aea65072
AnkitGurwan <ankitgurwan083@gmail.com>
2023-10-15 01:55:41 +0530

    Made UI Screen Responsive

--- frontend/src/components/Rules/RulesList/RulesListCard.js
+++ frontend/src/components/Rules/RulesList/RulesListCard.js
@@ -5,11 +5,11 @@ const RulesListCard = (props) => {
   const [dropDown,setDropDown] = useState(true);
 
   return (
-    <div className="w-2/3 rounded-lg shadow-lg py-0 my-8 mx-auto font-raleway flex" style={{"box-shadow" : "rgba(0, 0, 0, 0.24) 0px 3px 8px;"}}>
-      <div className='w-28 flex justify-center items-center border-r-2 py-auto' style={{"border-color" : "#D9D9D9"}}>{props.key}</div>
-      <div className='w-full h-full flex justify-between items-center mx-10 py-5'>
-        <div className='font-medium'>{props.header}</div>  
-        <div className='bg-reddishgray px-6 py-1 rounded-full font-light' style={{"box-shadow" : "rgba(0, 0, 0, 0.08) 0px 2px 4px;"}}>PDF</div>  
+    <div className="w-3/4 md:w-2/3 rounded-lg md:rounded-xl shadow-lg py-0 my-6 md:my-8 mx-auto font-raleway flex" style={{"box-shadow" : "rgba(0, 0, 0, 0.24) 0px 3px 8px;"}}>
+      <div className='w-24 md:w-28 flex justify-center items-center border-r-2 py-auto' style={{"border-color" : "#D9D9D9"}}>{props.key}</div>
+      <div className='w-full h-full flex justify-between items-center mx-8 md:mx-10 py-5'>
+        <div className='font-medium text-sm md:text-lg capitalize'>props.header</div>  
+        <div className='bg-reddishgray px-4 md:px-6 py-1 rounded-full font-light text-sm md:text-lg' style={{"box-shadow" : "rgba(0, 0, 0, 0.12) 0px 2px 4px;"}}>PDF</div>  
       </div>
     </div>
   )
--- frontend/src/components/Services/ServicesList/ServicesListCard.js
+++ frontend/src/components/Services/ServicesList/ServicesListCard.js
@@ -5,8 +5,8 @@ const ServicesListCard = (props) => {
   const [dropDown,setDropDown] = useState(true);
 
   return (
-    <div className="rounded-xl w-2/3 py-5 my-8 mx-auto bg-whitesmoke font-cataraman bg-reddishgray" style={{"box-shadow" : "rgba(0, 0, 0, 0.24) 0px 3px 8px;"}}>
-        <div className='flex justify-between items-center mx-12 font-semibold'>
+    <div className="rounded-lg md:rounded-xl w-3/4 md:w-2/3 py-4 md:py-5 my-6 md:my-8 mx-auto font-cataraman bg-reddishgray" style={{"box-shadow" : "rgba(0, 0, 0, 0.24) 0px 3px 8px;"}}>
+        <div className='flex justify-between items-center mx-8 md:mx-12 font-semibold'>
             <div>{props.header}</div>
             <div className='cursor-pointer p-2' onClick={()=>{setDropDown(!dropDown)}}>
               {dropDown
@@ -18,10 +18,10 @@ const ServicesListCard = (props) => {
             </div>
         </div>
         {!dropDown && props.subHeader?
-        <div className='mx-12 text-sm mt-2 text-darkblue'>
+        <div className='mx-8 md:mx-12 text-sm mt-2 text-darkblue'>
           {props.subHeader.map((subheader) => {
-            <Link to={subheader.url} className='my-3 flex items-center cursor-pointer'>
-              <div className='mr-3'>{subheader.name}</div>
+            <Link to={subheader.url} className='my-2 md:my-3 flex items-center cursor-pointer'>
+              <div className='mr-2 md:mr-3 capitalize'>{subheader.name}</div>
               <BsChevronRight/>
             </Link>
             })
