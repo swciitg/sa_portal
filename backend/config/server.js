2be1f35049724000cb380bf415f1ecae9137471e
Arpit Sureka <surekaarpit@gmail.com>
2023-10-22 00:01:37 +0530

    static url fixes

--- backend/config/server.js
+++ backend/config/server.js
@@ -4,7 +4,10 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  emitErrors: false,
+  url: env('PUBLIC_URL', 'https://api.example.com'),
+  proxy: env.bool('IS_PROXIED', true),
+  cron: {
+    enabled: env.bool('CRON_ENABLED', false),
   },
 });
