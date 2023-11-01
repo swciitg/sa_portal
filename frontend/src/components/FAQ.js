a84105faf9a02f780260fbd86c3fa1da4610f835
deepak-2605 <108828387+deepak-2605@users.noreply.github.com>
2023-11-01 22:07:39 +0530

    Minor fixes

--- frontend/src/components/FAQ.js
+++ frontend/src/components/FAQ.js
@@ -5,7 +5,7 @@ export default function LatestUpdates ({prop}) {
         <>
             <div className="flex grid rounded-xl shadow-xl lg:m-10 mr-10 mb-5 p-3 border-solid w-72 pr-8 ">
                 <div className=' p-2' >
-                    <span className='inline-block allign-middle text-gray-500 font-semibold text-lg '>Latest Updates</span>
+                    <span className='inline-block allign-middle text-gray-500 font-semibold text-l '>Latest Updates</span>
                 </div>
                 <div className='flex grid' >
                     <div className="inline-block w-0.5 self-stretch bg-gray-300 opacity-100 dark:opacity-50 col-start-2 col-end-3"></div>
