174d78f2171fa481f5e2b4184cabab9b6e769054
Raghav <raghav03.doda@gmail.com>
2023-11-05 20:20:44 +0530

    Corrected mistakes

--- frontend/src/components/Annocements.js
+++ frontend/src/components/Annocements.js
@@ -11,7 +11,7 @@ function classNames(...classes) {
 
 
 export default function Announcement ({prop}) {
-  const [opt,setopt] = useState("options")
+  const [opt,setopt] = useState("Important")
     return (
         <>
         <div className='flex lg:ms-10 mr-10 mt-5' style={{justifyContent:"space-between"}} >
@@ -34,51 +34,57 @@ export default function Announcement ({prop}) {
                   >
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-50 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <div className="py-1">
-                        <Menu.Item>
-                          {({ active }) => (
-                            <button
-                              onClick={()=>setopt("Important")}
-                              className={classNames(
-                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                'block px-4 py-2 text-sm'
-                              )}
-                            >
-                              Important
-                            </button>
-                          )}
-                        </Menu.Item>
-                        <Menu.Item>
-                          {({ active }) => (
-                            <button
-                              onClick={()=>setopt("Latest")}
-                              className={classNames(
-                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                'block px-4 py-2 text-sm'
-                              )}
-                            >
-                              Latest
-                            </button>
-                          )}
-                        </Menu.Item>
-                        <Menu.Item>
-                          {({ active }) => (
-                            <button
-                              onClick={()=>setopt("Old")}
-                              className={classNames(
-                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                'block px-4 py-2 text-sm'
-                              )}
-                            >
-                              Old
-                            </button>
-                          )}
-                        </Menu.Item>
+                        {prop.options.map((item)=>{
+                          return(
+                            <>
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <button
+                                    onClick={()=>setopt(item)}
+                                    className={classNames(
+                                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                      'block px-4 py-2 text-sm'
+                                    )}
+                                  >
+                                    {item}
+                                  </button>
+                                )}
+                              </Menu.Item>
+                            </>
+                          )
+                        })}
                       </div>
                     </Menu.Items>
                   </Transition>
             </Menu>
         </div>
-        {prop.Announcements.map((item)=>{
+        {opt=="Important" && prop.Important.map((item)=>{
+            return(
+            <div className="flex grid rounded-3xl shadow-xl lg:ms-10 lg:ml-10 mr-10 p-3 border-solid">
+                <div className="col-start-2 col-end-9">
+                    <p className="ps-3 pl-3 pt-3 text-md">{item.date}</p>
+                    <p className="p-3 text-sm font-semibold break-all  " >{item.description}</p>
+                </div>
+                <div className="flex col-start-10 col-end-12 text-2xl">
+                    <button className="inline-block allign-middle"><Link href={item.link} className='text-darkblue' ><BsFillArrowRightSquareFill className='rounded-lg' /></Link></button>
+                </div>
+            </div>
+            )
+        })}
+        {opt=="Latest" && prop.Latest.map((item)=>{
+            return(
+            <div className="flex grid rounded-3xl shadow-xl lg:ms-10 lg:ml-10 mr-10 p-3 border-solid">
+                <div className="col-start-2 col-end-9">
+                    <p className="ps-3 pl-3 pt-3 text-md">{item.date}</p>
+                    <p className="p-3 text-sm font-semibold break-all  " >{item.description}</p>
+                </div>
+                <div className="flex col-start-10 col-end-12 text-2xl">
+                    <button className="inline-block allign-middle"><Link href={item.link} className='text-darkblue' ><BsFillArrowRightSquareFill className='rounded-lg' /></Link></button>
+                </div>
+            </div>
+            )
+        })}
+        {opt=="Old" && prop.Old.map((item)=>{
             return(
             <div className="flex grid rounded-3xl shadow-xl lg:ms-10 lg:ml-10 mr-10 p-3 border-solid">
                 <div className="col-start-2 col-end-9">
