f368dbdc9e9befa10f593fc609299d0ea1e608ec
DEEPAK SINGH <108828387+deepak-2605@users.noreply.github.com>
2023-11-06 18:18:02 +0530

    Corrected mistakes (#20)
    Co-authored-by: Raghav <raghav03.doda@gmail.com>

--- frontend/src/components/FAQ.js
+++ frontend/src/components/FAQ.js
@@ -1,25 +1,18 @@
-import Link from "next/link"
+import { useState } from 'react';
+import FAQcard from './FAQcard'
 
-export default function LatestUpdates ({prop}) {
+export default function FAQs ({prop}) {
+    const [show, setShow] = useState(false);
+
+  const toggleShow = () => setShow(!show);
     return (
         <>
-            <div className="flex grid rounded-xl shadow-xl lg:m-10 mr-10 mb-5 p-3 border-solid w-72 pr-8 ">
-                <div className=' p-2' >
-                    <span className='inline-block allign-middle text-gray-500 font-semibold text-l '>Latest Updates</span>
-                </div>
-                <div className='flex grid' >
-                    <div className="inline-block w-0.5 self-stretch bg-gray-300 opacity-100 dark:opacity-50 col-start-2 col-end-3"></div>
-                    <div className="col-start-3 col-end-12" >
-                        {prop.FAQs.map((item)=>{
-                            return( 
-                            <div className="w-full m-2">
-                                <p className='text-blue break-all ms-2 text-xs font-medium'>{`${item.ques}`}</p>
-                            </div>
-                            )
-                        })}
-                    </div>
-                </div>
-            </div>     
+            <div >
+                <p className="text-4xl ms-[10%] mt-4 font-semibold font-[Manrope]" >FAQ</p>
+            </div>
+            {prop.questions.map((item) => (
+                    <FAQcard prop = {item}/>
+            ))}
         </>
     )
 }
