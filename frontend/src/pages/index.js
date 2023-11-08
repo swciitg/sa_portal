bc44d6b3b0aaccdbd0ee0052d3887de92158b641
deepak-2605 <108828387+deepak-2605@users.noreply.github.com>
2023-11-08 18:28:56 +0530

    Minor fixes

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,11 +1,9 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import HomePage from '../components/HomePage'
-import useFetch from '@/hooks/useFetch'
+
 const inter = Inter({ subsets: ['latin'] });
 
-const {data,error,loading}=useFetch('https://swc.iitg.ac.in/saPortal/api/teams?populate=deep');
-console.log(data);
 export default function Home() {
   return (
      <HomePage></HomePage>
