89a6d802b63f669d6fca6a62fb5c27b9053b15f4
AnkitGurwan <ankitgurwan083@gmail.com>
2023-12-05 00:32:02 +0530

    Added serviceslist

--- frontend/src/components/Services/ServicesList/ServicesListCard.js
+++ frontend/src/components/Services/ServicesList/ServicesListCard.js
@@ -4,9 +4,10 @@ import { BsChevronDown ,  BsChevronUp ,BsChevronRight } from 'react-icons/bs'
 
 const ServicesListCard = (props) => {
   const [dropDown,setDropDown] = useState(true);
+  
 
   return (
-    <div className="rounded-lg md:rounded-xl w-3/4 md:w-2/3 py-4 md:py-5 my-6 md:my-8 mx-auto font-Manrope bg-reddishgray" style={{"box-shadow" : "rgba(0, 0, 0, 0.24) 0px 3px 8px;"}}>
+    <div className="rounded-lg md:rounded-xl w-3/4 py-4 md:py-5 my-6 md:my-8 mx-auto font-Manrope md:ml-0" style={{"box-shadow" : "rgba(0, 0, 0, 0.24) 0px 3px 8px","backgroundColor":"rgba(252, 252, 253, 1)"}}>
         <div className='flex justify-between items-center mx-8 md:mx-12 font-semibold tracking-wide text-[16px]'>
             <div>{props.header}</div>
             <div className='cursor-pointer p-2' onClick={()=>{setDropDown(!dropDown)}}>
@@ -21,8 +22,8 @@ const ServicesListCard = (props) => {
         {!dropDown && props.subHeader?
         <div className='mx-8 md:mx-12 text-sm mt-2 text-darkblue'>
           {props.subHeader.map((subheader) => {
-            return <Link href={subheader.url} className='my-2 md:my-3 flex items-center cursor-pointer'>
-                      <div className='mr-2 md:mr-3 capitalize font-normal tracking-wide'>{subheader.name}</div>
+            return <Link href={subheader.URL} className='my-2 md:my-3 flex items-center cursor-pointer'>
+                      <div className='mr-2 md:mr-3 capitalize font-normal tracking-wide'>{subheader.Sub_Header}</div>
                       <BsChevronRight/>
                   </Link>
             })
---
+++ frontend/src/pages/Services/index.js
@@ -0,0 +1,19 @@
+import Footer from '@/components/Footer/Footer';
+import Navbar from '@/components/Navbar/Navbar';
+import ServicesList from '@/components/Services/index';
+import React from 'react';
+
+const index = () => {
+  return (
+    <div className='min-h-full absolute w-full'>
+      <Navbar/>
+      <ServicesList/>
+      <div className='relative bottom-0 w-full'>
+        <Footer/>
+      </div>
+    </div>
+    
+  )
+}
+
+export default index
