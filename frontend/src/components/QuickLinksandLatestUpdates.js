3b378525b5aa20d9aa0a9759c23ae1ba8c569249
Akshat2403 <95174507+Akshat2403@users.noreply.github.com>
2023-12-06 14:21:39 +0530

    change route to /home

--- frontend/src/components/QuickLinksandLatestUpdates.js
+++ frontend/src/components/QuickLinksandLatestUpdates.js
@@ -3,7 +3,7 @@ import QuickLinks from "./QuickLinks";
 const QuickLinksAndLatestUpdates = ({ prop }) => {
   return (
     <>
-      <div className="flex flex-col lg:flex-row">
+      <div className="flex flex-col lg:justify-between lg:flex-row">
         <QuickLinks prop={prop} />
         <LatestUpdates prop={prop} />
       </div>
