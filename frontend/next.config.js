435a64246fdfc1f05d82003177fb2fbcdb3a336b
Raghav <raghav03.doda@gmail.com>
2023-12-28 12:07:39 +0530

    Revert "Teams page backend connected"
    This reverts commit b8548e379a1f647aee6b4330f58acde1ba8f1e4f.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images : {
-    domains:["swc.iitg.ac.in"]
+    domains:["images.pexels.com"]
   }
 }
 
--- frontend/src/components/teams/Card.js
+++ frontend/src/components/teams/Card.js
@@ -19,13 +19,11 @@ export default function Card ({prop}) {
         setTimeout(() => {setIcon2(<MdOutlineContentCopy/>)},"3000")
     } 
 
-    console.log(prop.Profile_Photo.data.attributes.formats.medium.url)
-
     return (
         <>
             <div className="rounded overflow-hidden shadow-lg max-w-sm">
                 <Image
-                src={`https://swc.iitg.ac.in/saPortal${prop.Profile_Photo.data.attributes.formats.medium.url}`}
+                src={prop.image}
                 alt="image"
                 className="w-full"
                 width = "300"
@@ -33,23 +31,23 @@ export default function Card ({prop}) {
                 ></Image>
                 <div className="px-6-py-4">
                     <p className="font-bold text-2xl px-4 py-2">{prop.name}</p>
-                    <p className="text-xl-mb-2 text-gray-400 px-4 py-2">{prop.Designation}</p>
+                    <p className="text-xl-mb-2 text-gray-400 px-4 py-2">{prop.designation}</p>
                 </div>
                 <div className=" pb-2 px-4 " >
-                    {prop.Issues.map((button)=>{
+                    {prop.buttons.map((button)=>{
                         return (
-                            <button key={button.id} style={{backgroundColor:"rgb(167 139 250)"}} className=" hover:cursor-pointer rounded-full px-3 py-0.5 text-xs font-base mx-0.5 mb-px">{button.Value}</button>
+                            <button key={button.id} style={{backgroundColor:"rgb(167 139 250)"}} className=" hover:cursor-pointer rounded-full px-3 py-0.5 text-xs font-base mx-0.5 mb-px">{button.value}</button>
                         )
                     })}
                     <hr className="solid text-gray-500 my-1 " />
                 </div>
                 <div>   
                     <div className="flex" >
-                        <span className="text-gray-400 px-4 " >{prop.Email}</span>
+                        <span className="text-gray-400 px-4 " >{prop.link}</span>
                         <span className="hover:cursor-pointer" onClick={clickHandler} >{icon}</span>
                     </div>                    
                     <div className="flex" >
-                        <span className="text-gray-400 px-4 " >{`+91 ${prop.Phone_Number}`}</span>
+                        <span className="text-gray-400 px-4 " >{`+91 ${prop.phone}`}</span>
                         <span className="hover:cursor-pointer" onClick={clickHandler2} >{icon2}</span>
                     </div>
                 </div>
