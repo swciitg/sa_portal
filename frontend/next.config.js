340e6220c63205075d115ec417faddf6766a2536
swc <web_swc@iitg.ac.in>
2024-01-07 07:48:14 +0000

    updated config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/saportalnew',
   reactStrictMode: true,
   images: {
     domains: ["images.pexels.com", "swc.iitg.ac.in"],
+  },
+  distDir: "_next",
+  generateBuildId: async () => {
+    if (process.env.BUILD_ID) {
+      return process.env.BUILD_ID;
+    } else {
+      return `${new Date().getTime()}`;
+    }
   },
 };
 
