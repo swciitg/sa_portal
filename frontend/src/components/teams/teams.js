9fc30f94aca1740d583446c9252c8745db1b4418
Raghav <raghav03.doda@gmail.com>
2024-01-07 22:40:17 +0530

    minor changes

--- frontend/src/components/teams/teams.js
+++ frontend/src/components/teams/teams.js
@@ -1,8 +1,30 @@
 import Card from './Card'
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 
-const teams = ({prop}) => {
 
+const teams = () => {
+    const [data, setData] = useState("");
+  
+    const getItem = async () => {
+      const response = await fetch(`https://swc.iitg.ac.in/saPortal/api/teams?populate=deep`, {
+          method: 'GET',
+          headers: {
+              'Content-Type': 'application/json',
+          }
+      });
+      
+      const json = await response.json();
+      setData(json);
+    }
+    
+    useEffect(()=>{
+      getItem();
+    
+    },[])
+    
+    const prop = data
+        // console.log(data);
+    
     const [opt,setOpt] = useState(1)
     if (prop.data && prop.data.length > 0 && prop.data[0].attributes && prop.data[0].attributes.Team_Members && prop.data[0].attributes.Team_Members.length > 0) {
         return (
--- frontend/src/pages/teams/index.js
+++ frontend/src/pages/teams/index.js
@@ -2,35 +2,15 @@ import React from 'react'
 import Teams from '../../components/teams/teams'
 import Footer from '@/components/Footer/Footer.js'
 import Navbar from '@/components/Navbar/Navbar'
-import { useState,useEffect } from 'react'
 
 
 const index = () => {
-    const [data, setData] = useState("");
-  
-    const getItem = async () => {
-      const response = await fetch(`https://swc.iitg.ac.in/saPortal/api/teams?populate=deep`, {
-          method: 'GET',
-          headers: {
-              'Content-Type': 'application/json',
-          }
-      });
-      
-      const json = await response.json();
-      setData(json);
-    }
-  
-    useEffect(()=>{
-      getItem();
-
-    },[])
-
-        // console.log(data);
+   
   return (
     <>
     <div>
         <Navbar/>
-        <Teams prop = {data} />
+        <Teams />
         <Footer/>
     </div>
     </>
