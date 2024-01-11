a703682e8c6628c652788f8ad01279f5ebccf154
swc <web_swc@iitg.ac.in>
2024-01-11 12:41:37 +0000

    changes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,18 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/saportalnew',
-  reactStrictMode: true,
-  images: {
-    domains: ["images.pexels.com", "swc.iitg.ac.in"],
-  },
-  distDir: "_next",
-  generateBuildId: async () => {
-    if (process.env.BUILD_ID) {
-      return process.env.BUILD_ID;
-    } else {
-      return `${new Date().getTime()}`;
-    }
-  },
+  reactStrictMode: true
 };
 
 module.exports = nextConfig;
