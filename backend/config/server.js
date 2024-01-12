5cd3b039f68593f0ee17c4c3846ddb7cd4b875e6
swc <web_swc@iitg.ac.in>
2024-01-12 10:55:34 +0000

    changes

--- backend/config/server.js
+++ backend/config/server.js
@@ -5,7 +5,11 @@ module.exports = ({ env }) => ({
     keys: env.array('APP_KEYS'),
   },
   emitErrors: false,
+<<<<<<< HEAD
   url: 'https://swc.iitg.ac.in/sa_portal_backend/',
+=======
+  url: 'https://swc.iitg.ac.in/sa_portal_backend',
+>>>>>>> 3b75c2a (changes)
   proxy: env.bool('IS_PROXIED', true),
   cron: {
     enabled: env.bool('CRON_ENABLED', false),
