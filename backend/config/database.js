ff0b0608b90fc90e919c1e89d8ffc94745af141c
Arpit Sureka <surekaarpit@gmail.com>
2024-01-13 18:12:23 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,13 +2,13 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: env.array('DATABASE_CLIENT'),
+    client: env('DATABASE_CLIENT'),
     connection: {
-      host: env.array('DATABASE_HOST'),
-      port: env.array('DATABASE_PORT'),
-      database: env.array('DATABASE_NAME'),
-      user: env.array('DATABASE_USERNAME'),
-      password: env.array('DATABASE_PASSWORD'),
+      host: env('DATABASE_HOST'),
+      port: env('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
       schema: 'public', // Not required
       ssl:  false,
     },
