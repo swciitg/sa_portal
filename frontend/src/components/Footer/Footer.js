ac3aa6b8264fa3a48e9763eb1eb5339f833a1cff
Arpit Sureka <surekaarpit@gmail.com>
2024-01-25 17:00:36 +0530

    Revert "url edit"
    This reverts commit afcc84434e1a637b5bd0e94e9a5eaed4d72cb122.

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -21,7 +21,7 @@ const Footer = () => {
       >
         <div className="flex items-center ml-6 md:ml-14 lg:ml-16 md:w-7/12 w-full">
           <Image
-            src="images/IITG_Logo_svg.svg"
+            src="/images/IITG_Logo_svg.svg"
             width={1}
             height={1}
             className="flex lg:w-20 md:w-16 md:h-16 lg:h-20 h-20 w-20 p-1"
@@ -57,13 +57,13 @@ const Footer = () => {
             Get Connected
             <div className="flex flex-row mt-1">
               <Image
-                src="icons/Twitter.svg"
+                src="/icons/Twitter.svg"
                 width={1}
                 height={1}
                 className="flex lg:w-7 lg:h-7 md:w-6 md:h-6 w-7 h-7  md:mr-5 mr-3 "
               />
               <Image
-                src="icons/Linkedin.svg"
+                src="/icons/Linkedin.svg"
                 width={1}
                 height={1}
                 className="flex lg:w-7 lg:h-7 md:w-6 md:h-6 h-7 w-7 md:p-0.5 p-1"
@@ -96,7 +96,7 @@ const Footer = () => {
       <div className="flex flex-row bg-[#101323] h-16 items-center">
         <div className="flex lg:ml-20 md:ml-16 ml-10">
           <Image
-            src="icons/SWC_Logo.svg"
+            src="/icons/SWC_Logo.svg"
             width={1}
             height={1}
             className="flex md:h-6 md:w-6 h-5 w-5"
