6cb8d8360c4fb4ac8b689f55e850454768920983
swc <web_swc@iitg.ac.in>
2024-02-04 16:55:27 +0000

    changes

---
+++ backend/src/api/sac/controllers/sac.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sac controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::sac.sac');
---
+++ backend/src/api/sac/routes/sac.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sac router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::sac.sac');
