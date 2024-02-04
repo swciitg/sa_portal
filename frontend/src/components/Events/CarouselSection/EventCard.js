3ce3a26698e58999408a6e3faf9ace5d679623a2
deepak-2605 <108828387+deepak-2605@users.noreply.github.com>
2024-02-04 14:05:01 +0530

    Minor changes

--- frontend/src/components/Events/CarouselSection/EventCard.js
+++ frontend/src/components/Events/CarouselSection/EventCard.js
@@ -8,7 +8,7 @@ const Card = ({ image, title, icon, description, link }) => {
         <>
             <a href={link}>
             <div className={`bg-white-100 rounded-lg overflow-hidden mx-2 snap-center w-[350px] sm:w-[600px] h-72 my-8`} onMouseEnter={() => setHoving(true)} onMouseLeave={() => setHoving(false)}>
-                <img className={hoving ? "w-full h-full blur-sm" : "w-full h-full"} src={image} alt="Card Image" />
+                <img className={hoving ? "w-full h-full blur-sm object-cover" : "w-full h-full object-cover"} src={image} alt="Card Image" />
                 <div className={`transform ${hoving ? (description.length > 600 ? '-translate-y-52' : (description.length > 400 ? '-translate-y-40' : (description.length > 200 ? '-translate-y-32' : '-translate-y-24'))) : '-translate-y-12'} bg-slate-600 bg-opacity-60 duration-300 w-full h-full justify-center items-end text-white font-semibold bg-[#182133] pt-1`}>
                     <div className="flex px-6">
                         <p className="text-2xl font-semibold">{title}</p>
--- frontend/src/components/teams/teams.js
+++ frontend/src/components/teams/teams.js
@@ -1,61 +1,62 @@
-import Card from './Card'
-import { useState,useEffect } from 'react'
+import Card from './Card';
+import { useState, useEffect } from 'react';
 import { Manrope } from "next/font/google";
-const manrope = Manrope({ subsets: ["latin"], weight: ["600", "400","500","700","800","200"] });
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
+
+const manrope = Manrope({ subsets: ["latin"], weight: ["600", "400", "500", "700", "800", "200"] });
+
 const Teams = () => {
-        const [prop, setProp] = useState("");
+  const [prop, setProp] = useState("");
+  const [opt, setOpt] = useState(1);
 
-    const getItem = async () => {
-      const response = await fetch(`https://swc.iitg.ac.in/sa_portal_backend/api/teams?populate=deep`, {
-          method: 'GET',
-          headers: {
-              'Content-Type': 'application/json',
-          }
-      });
+  const getItem = async () => {
+    const response = await fetch(`https://swc.iitg.ac.in/sa_portal_backend/api/teams?populate=deep`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
 
-      const json = await response.json();
-      setProp(json);
-    }
+    const json = await response.json();
+    setProp(json);
+  }
 
-    useEffect(()=>{
-      getItem();
+  useEffect(() => {
+    getItem();
+  }, [])
 
-    },[])
-    const [opt,setOpt] = useState(1)
-    if (prop.data && prop.data.length > 0 && prop.data[0].attributes && prop.data[0].attributes.Team_Members && prop.data[0].attributes.Team_Members.length > 0) {
-        return (
-            <>
-                <div className={`flex flex-wrap w-[80%] gap-6 text-xs py-2 ${manrope.className} mx-[8%] `} >
-                    {prop.data.map((button) => {
+  if (prop.data && prop.data.length > 0 && prop.data[0].attributes && prop.data[0].attributes.Team_Members && prop.data[0].attributes.Team_Members.length > 0) {
     return (
-        <button
-            key={button.id} 
-            className=" hover:bg-[#1E2532] hover:text-white  sm:py-2 py-3 px-[3%] font font-medium  text-xs sm:text-sm flex items-center justify-center  shadow-lg rounded-xl box-border"
-            onClick={() => { setOpt(`${button.id}`) }}
-        >
-            {`${button.attributes.Name}`}
-        </button>
-    )
-})}
-
-                </div>
-                
-                <div className={`mx-[7%] my-[5%] gap-x-8 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 flex p-4 ${manrope.className}`} >
-                   {prop.data[opt - 1].attributes.Team_Members.map((item) => {
-    return (
-        <div key={item.id} className="mt-[5%]">
-            <Card prop={item} />
+      <>
+        <div className={`flex flex-wrap w-[80%] gap-6 text-xs py-2 ${manrope.className} mx-[8%] `}>
+          {prop.data.map((button, index) => (
+            <button
+              key={button.id}
+              className={`${
+                opt === index + 1
+                  ? 'bg-[#1e2532] text-white'
+                  : 'bg-white text-black'
+              } sm:py-2 py-3 px-[3%] font font-medium text-xs sm:text-sm flex items-center justify-center shadow-lg rounded-xl box-border`}
+              onClick={() => { setOpt(index + 1) }}
+            >
+              {button.attributes.Name}
+            </button>
+          ))}
         </div>
-    );
-})}
 
-                </div>
-            </>
-        )
-    } else {
-        console.log('Data structure is not as expected');
-    }
-    
+        <div className={`mx-[7%] my-[5%] gap-x-8 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 flex p-4 ${manrope.className}`}>
+          {prop.data[opt - 1].attributes.Team_Members.map((item) => (
+            <div key={item.id} className="mt-[5%]">
+              <Card prop={item} />
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  } else {
+    console.log('Data structure is not as expected');
+    return null; // Add your error handling or loading indicator here if needed
+  }
 }
 
-export default Teams
+export default Teams;
