0776ac78df4b4ed93a7e79a03db8b36681d1bcd6
Durgesh Kumar Deo <durgeshdeo1410@gmail.com>
2024-10-28 11:56:49 +0530

    fixes

--- frontend/src/components/Events/CarouselSection/EventCarousel.js
+++ frontend/src/components/Events/CarouselSection/EventCarousel.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import Card from './EventCard';
-import Image from 'next/image';
-import { Splide, SplideSlide } from 'splide-nextjs/react-splide';
-import 'splide-nextjs/splide/dist/css/themes/splide-default.min.css';
+import React from "react";
+import Card from "./EventCard";
+import { Splide, SplideSlide } from "splide-nextjs/react-splide";
+import "splide-nextjs/splide/dist/css/themes/splide-default.min.css";
 
 const Carousel = ({ props }) => {
-  // console.log(props[0]);
-
   const getWindowWidth = () => {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== "undefined") {
       return window.innerWidth;
     }
     return 0;
@@ -17,38 +14,46 @@ const Carousel = ({ props }) => {
   const isMobile = getWindowWidth() <= 450;
 
   return (
-    <>
-    <div className=' mx-[4%] sm:mx-[8%]'>
+    <div className="carousel-container">
       <Splide
         options={{
-          fixedWidth: 600,
+          fixedWidth: isMobile ? 200 : 600,
           perMove: 1,
           autoplay: true,
           gap: 25,
-          lazyLoad: 'nearby',
-          autowidth: true,
-          autpheight: true,
-         
-          
-          isLoop: true,
+          lazyLoad: "nearby",
+          autoWidth: true,
+          autoHeight: true,
         }}
       >
-        {props.map((prop) => {
-          return (
-            <SplideSlide key={prop?.id}>
-              <Card
-                image={`${process.env.NEXT_PUBLIC_API_ENDPOINT}` + prop?.Image?.data[0]?.attributes?.url}
-                title={prop?.Title}
-                icon={`${process.env.NEXT_PUBLIC_API_ENDPOINT}` + prop?.Icon?.data[0]?.attributes?.url}
-                description={prop?.description}
-                link={prop?.Link}
-              />
-            </SplideSlide>
-          );
-        })}
+        {props.map((prop) => (
+          <SplideSlide key={prop?.id}>
+            <Card
+              image={
+                `${process.env.NEXT_PUBLIC_API_ENDPOINT}` +
+                prop?.Image?.data[0]?.attributes?.url
+              }
+              title={prop?.Title}
+              icon={
+                `${process.env.NEXT_PUBLIC_API_ENDPOINT}` +
+                prop?.Icon?.data[0]?.attributes?.url
+              }
+              description={prop?.description}
+              link={prop?.Link}
+            />
+          </SplideSlide>
+        ))}
       </Splide>
-      </div>
-    </>
+
+      <style jsx>{`
+        .carousel-container {
+          max-width: 98.5vw;
+          overflow: hidden;
+          padding: 0 20px;
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
   );
 };
 
--- frontend/src/components/Events/index.js
+++ frontend/src/components/Events/index.js
@@ -1,50 +1,50 @@
-import React ,{useEffect,useState} from 'react';
-import About from './About/about';
-import EventCarousel from './CarouselSection/EventCarousel'
+import React, { useEffect, useState } from "react";
+import About from "./About/about";
+import EventCarousel from "./CarouselSection/EventCarousel";
 import { Manrope } from "next/font/google";
-const manrope = Manrope({ subsets: ["latin"], weight: ["600", "400","700"] });
+const manrope = Manrope({ subsets: ["latin"], weight: ["600", "400", "700"] });
 const Index = () => {
   const [data, setData] = useState("");
 
   const getItem = async () => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/events?populate=deep`, {
-        method: 'GET',
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/events?populate=deep`,
+      {
+        method: "GET",
         headers: {
-            'Content-Type': 'application/json',
-        }
-    });
-    
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
     const json = await response.json();
     setData(json?.data[0]);
     // console.log(json.data[0].attributes.Technical_Events[0].Icon.data[0].attributes.url,"hello");
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     getItem();
-  },[])
-
-    
+  }, []);
 
   return (
     <div className={manrope.className}>
-        { <About id={data?.id} content={data?data?.attributes?.About:""}/> }
-       <div className=' mx-[15%]    text-3xl  py-6 md:py-8 font-bold text-[#231F20]'>
-            Technical 
-        </div>
+      {<About id={data?.id} content={data ? data?.attributes?.About : ""} />}
+      <div className=" mx-[15%]    text-3xl  py-6 md:py-8 font-bold text-[#231F20]">
+        Technical
+      </div>
 
-        {data && data?.attributes?.Technical_Events && (
-  <EventCarousel props={data?.attributes?.Technical_Events} />
-)}
-  <div className='text-3xl  py-6 md:py-8 mx-[15%]  font-bold  text-[#231F20] '>
-            Cultural 
-        </div>
+      {data && data?.attributes?.Technical_Events && (
+        <EventCarousel props={data?.attributes?.Technical_Events} />
+      )}
+      <div className="text-3xl  py-6 md:py-8 mx-[15%]  font-bold  text-[#231F20] ">
+        Cultural
+      </div>
 
-        {data && data?.attributes?.Cultural_Events && (
-  <EventCarousel props={data?.attributes?.Cultural_Events} />
-)}
-        
+      {data && data?.attributes?.Cultural_Events && (
+        <EventCarousel props={data?.attributes?.Cultural_Events} />
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Index;
